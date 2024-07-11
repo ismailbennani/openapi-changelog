50d1879110604aabf620b91fff3c76cb4fbca401
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-07-11 22:49:02 +0200

    simplify

--- src/cli.ts
+++ src/cli.ts
@@ -1,55 +1,75 @@
 #!/usr/bin/env node
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
-import { ArgumentsCamelCase, Argv } from "yargs";
+import { ArgumentsCamelCase } from "yargs";
 import winston, { format } from "winston";
 import * as util from "node:util";
-import { DiffArgs, diffCliHandler } from "./diff-cli.js";
+import fs from "node:fs/promises";
+import { diff } from "./diff/diff.js";
 
-winston.add(new winston.transports.Console({ format: format.combine(format.colorize(), format.simple()) }));
+winston.add(new winston.transports.Console({ format: format.combine(format.colorize(), format.simple()), stderrLevels: ["error", "warn", "info"], level: "info" }));
 
 const yargsInstance = yargs(hideBin(process.argv));
 const terminalWidth = yargsInstance.terminalWidth();
-const maxWidth = 120;
+const maxWidth = 140;
 
 await yargsInstance
+  .wrap(terminalWidth > maxWidth ? maxWidth : terminalWidth)
   .scriptName("openapi-changelog")
-  .usage("Usage: $0 <diff|changelog> [options]")
-  .command<DiffArgs>(
-    "diff <old_openapi_spec> <new_openapi_spec>",
-    "Get the differences between two open api specifications",
-    (yargs: Argv) => {
+  .usage(
+    "$0 <old_openapi_spec> <new_openapi_spec> [options]",
+    "Compute the changelog between the old and the new openapi specifications",
+    (yargs) =>
       yargs
         .positional("old_openapi_spec", {
           describe: "Path or url to the old openapi specification",
           type: "string",
+          demandOption: true,
+          normalize: true,
         })
-        .positional("new_openapi_spec", { describe: "Path or url to the new openapi specification", type: "string" })
+        .positional("new_openapi_spec", {
+          describe: "Path or url to the new openapi specification",
+          type: "string",
+          demandOption: true,
+          normalize: true,
+        })
+        .boolean("diff")
+        .describe("diff", "Output a JSON containing the differences between the two specifications instead of a nicely formatted changelog")
+        .group("diff", "Diff options")
         .string("output")
         .alias("o", "output")
-        .describe("output", "Output file name")
-        .default("output", "diff.json")
-        .group("output", "Diff options")
-        .usage("Usage: $0 diff <old_openapi_spec> <new_openapi_spec> --output diff.json [options]");
+        .describe("output", "Write the result to the corresponding file instead of stdout")
+        .group("output", "Changelog options")
+        .string("verbose")
+        .describe("verbose", "Print more stuff")
+        .group("verbose", "Common options")
+        .help("h")
+        .alias("h", "help")
+        .group("help", "Common options")
+        .group("version", "Common options")
+        .middleware([verboseMiddleware]),
+    async (args) => {
+      winston.info(`Computing diff\n\tfrom:\t ${args.old_openapi_spec}\n\tto:\t ${args.new_openapi_spec}`);
+
+      const result = await diff(args.old_openapi_spec, args.new_openapi_spec);
+      const formattedResult = args.diff === true ? JSON.stringify(result, null, 2) : "CHANGELOG (not implemented)";
+
+      if (args.output !== undefined) {
+        winston.info(`Writing result to ${args.output}`);
+        await fs.writeFile(args.output, formattedResult, { encoding: "utf8", flag: "w" });
+        winston.info(`Done.`);
+      } else {
+        process.stdout.write(formattedResult);
+      }
     },
-    diffCliHandler,
   )
-  .boolean("verbose")
-  .describe("verbose", "Print more stuff")
-  .group("verbose", "Common options")
-  .help("h")
-  .alias("h", "help")
-  .group("help", "Common options")
-  .group("version", "Common options")
-  .wrap(terminalWidth > maxWidth ? maxWidth : terminalWidth)
-  .middleware([verboseMiddleware])
   .parse();
 
-function verboseMiddleware(args: ArgumentsCamelCase<{ verbose: boolean | undefined }>): void {
-  if (args.verbose === true) {
-    winston.level = "debug";
+function verboseMiddleware(args: ArgumentsCamelCase<{ verbose: string | undefined }>): void {
+  if (args.verbose !== undefined) {
+    const fileName = args.verbose === "" ? "debug.log" : args.verbose;
+    winston.info(`Verbose: writing debug logs to ${fileName}`);
+    winston.add(new winston.transports.File({ filename: fileName, format: format.combine(format.uncolorize(), format.simple()), level: "debug", options: { flags: "w" } }));
     winston.debug(`ARGS: ${util.inspect(args, false, null, true)}`);
   }
-
-  winston.level = "info";
 }

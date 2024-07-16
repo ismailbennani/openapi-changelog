f7ac8b0657abe036472b096a7a307cb942743c57
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-07-16 22:49:32 +0200

    fix failure when version is not a valid semantic version

--- src/core/openapi-documents-utils.ts
+++ src/core/openapi-documents-utils.ts
@@ -2,5 +2,11 @@ import { OpenAPIV3 } from "openapi-types";
 import semver from "semver";
 
 export function sortDocumentsByVersionDescInPlace(documents: OpenAPIV3.Document[]): OpenAPIV3.Document[] {
-  return documents.sort((a, b) => -semver.compareBuild(a.info.version, b.info.version, { loose: true }));
+  const semverOptions = { loose: true };
+
+  if (documents.some((d) => semver.valid(d.info.version, semverOptions) === null)) {
+    return documents.sort((a, b) => -a.info.version.localeCompare(b.info.version));
+  }
+
+  return documents.sort((a, b) => -semver.compareBuild(a.info.version, b.info.version, semverOptions));
 }
